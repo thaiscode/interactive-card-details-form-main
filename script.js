57e40c60c5144b38bf9df366766dde23db53d863
Thais Silva <thais_ap@live.com>
2022-11-19 02:23:04 -0300

    script fixed
    issues with validation of card number fixed

--- script.js
+++ script.js
@@ -2,56 +2,100 @@ let button = document.querySelector(".form__button")
 
 button.addEventListener("click", validation)
 
+/* Inputs */
 let inputName = document.querySelector('#form__card-name')
 let cardNumber = document.querySelector('#form__card-number')
 let cardDateMM = document.querySelector('#form__card-date--MM')
 let cardDateYY = document.querySelector('#form__card-date--YY')
 let cvc = document.querySelector('#form__card-CVC')
 
-
+/* error boxes */
 let errorName = document.querySelector('.error-box-name')
-let errorCardNumer = document.querySelector('.error-box-cardNumber')
+let errorCardNumber = document.querySelector('.error-box-cardNumber')
 let errorDate = document.querySelector('.error-box-date')
 let errorCVC = document.querySelector('.error-box-cvc')
 
+inputName.addEventListener('input', function(e) {
+    let  nameCard = document.querySelector('.card-front__name')
+    nameCard.innerHTML = e.target.value 
+})
+
+cardNumber.addEventListener('input', function(e) {
+   let numberCard = document.querySelector('.card-front__number')
+    numberCard.innerHTML = e.target.value 
+})
+
+cardDateMM.addEventListener('input', function(e) {
+    let mounthCard = document.querySelector('.card-front__mounth')
+    mounthCard.innerHTML = e.target.value 
+})
+
+cardDateYY.addEventListener('input', function(e) {
+    let yearCard = document.querySelector('.card-front__year')
+    yearCard.innerHTML = e.target.value 
+})
+
+cvc.addEventListener('input', function(e) {
+    let cvcCard = document.querySelector('.card-section__cvc')
+    cvcCard.innerHTML = e.target.value 
+})
+
+/* Validation */
 function validation() {
     if (inputName.value == "") {
         inputName.style.border = '1px solid hsl(0, 100%, 66%)'
+        inputName.style.marginBottom = '0'
         errorName.innerHTML = "Can´t be blank"
-    }
-
-    if (cardNumber.value == "") {
-        cardNumber.style.border = '1px solid hsl(0, 100%, 66%)'
-        errorCardNumer.innerHTML = "Can´t be blank"
-    } else if (!cardNumber.checkValidity()) {
-        errorCardNumer.innerHTML = "Wrong format, numbers only"
-    }
+    } 
 
     if (cardDateMM.value == "") {
         cardDateMM.style.border = '1px solid hsl(0, 100%, 66%)'
+        cardDateMM.style.marginBottom = '0'
         errorDate.innerHTML = "Can´t be blank"
-    }
+    } 
 
     if (cardDateYY.value == "") {
         cardDateYY.style.border = '1px solid hsl(0, 100%, 66%)'
+        cardDateYY.style.marginBottom = '0'
         errorDate.innerHTML = "Can´t be blank"
-    }
+    } 
 
     if (cvc.value == "") {
         cvc.style.border = '1px solid hsl(0, 100%, 66%)'
+        cvc.style.marginBottom = '0'
         errorCVC.innerHTML = "Can´t be blank"
+    } 
+    
+    if (cardNumber.value == "") {
+        cardNumber.style.border = '1px solid hsl(0, 100%, 66%)'
+        cardNumber.style.marginBottom = '0'
+        
+        errorCardNumber.innerHTML = "Can´t be blank"
+        errorCardNumber.style.marginBottom = '0.5rem'
+    } else if (!cardNumber.checkValidity()) {
+        cardNumber.style.border = '1px solid hsl(0, 100%, 66%)'
+        cardNumber.style.marginBottom = '0'
+        errorCardNumber.style.marginBottom = '0.5rem'
+        errorCardNumber.innerHTML = "Wrong format, numbers only"
     } else {
         thanksState()
     }
 }
 
+/*Thank you state*/ 
 function thanksState() {
-    let thanksState = document.querySelector('.thanks-state')
     let formSection = document.querySelector('.form-section')
-
+    let thanksState = document.querySelector('.thanks-state')
     formSection.style.display = 'none'
     thanksState.style.display = 'block'
 }
+
+/* Continue button*/
+let continueBTN = document.querySelector('.thanks-state__button')
+    continueBTN.addEventListener('click', function() {
+        window.location.reload()
+    })
+
 
 
 
